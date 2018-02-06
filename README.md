3c6fa0143269080404c4cecb548b4bc04fe4ecb2
ImJustTatan <sebastianrh27.12@gmail.com>
2018-02-06 00:56:25 -0300

    Added HP, more functions, etc.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Borg
 ## A fun little Text Adventure parody project
-[![Chat on discord](https://img.shields.io/badge/chat-on%20discord-blue.svg "Tatan's Server")](https://discord.gg/23jN7Na) ![Python Version](https://img.shields.io/badge/python-3.X-yellow.svg "Python Version") ![Version](https://img.shields.io/badge/version-v0.1a-brightgreen.svg "Version")
+[![Chat on discord](https://img.shields.io/badge/chat-on%20discord-blue.svg "Tatan's Server")](https://discord.gg/23jN7Na) ![Python Version](https://img.shields.io/badge/python-3.X-yellow.svg "Python Version") ![Version](https://img.shields.io/badge/version-v0.1b-brightgreen.svg "Version")
 ---
 ### Description
 Borg is a current WIP that tries to parody a lot of the text adventures from the 90s. As I mentioned, it's a WIP, and a really, ***really*** early one. Right now it doesn't even have more than an enemy.
--- main.py
+++ main.py
@@ -33,26 +33,41 @@ with open("sphrases.json") as sph: # sphrases = special phrases
 
 class Enemy(object):
  	"""Enemies (and sometimes not-so-enemies) you find in the adventure."""
- 	def __init__(self, name, desc, state):
+ 	def __init__(self, name, desc, hp, state):
  		self.name = name
  		self.desc = desc
+ 		self.hp = hp
  		if state == None:
  			self.state = str(state).lower()
  		else:
  			self.state = 'alive'
+ 		if self.hp <= 0:
+ 			self.state == 'dead'
+ 		else:
+ 			self.state == 'alive'
  	def chstate(self, s):
  		"""Changes the enemy's state to the one given by the parameter 's'."""
  		self.state = s.lower()
  		if s == 'dead':
+ 			self.hp = 0
  			cprint('\nYou killed the {}.'.format(self.name), 'red', attrs=['bold'])
+ 		else:
+ 			self.hp = self.hp
  	def shstate(self):
  		"""Displays the enemy's state."""
  		if self.state == 'dead':
- 			print('\nThe {} is dead. {}.'.format(self.name, sphrases["dstatements"][random.randint(0, 4)]))
+ 			cprint('\nThe {} is dead. {}.'.format(self.name, sphrases["dstatements"][random.randint(0, 4)]), attrs=['bold'])
  		elif self.state == 'alive':
- 			print('\n{}'.format(self.desc))
+ 			cprint('\n{}'.format(self.desc), attrs=['bold'])
  		else:
  			cprint('An error occured when trying to show this enemy\'s state.')
+ 	def ghit(self, hpm1, hpm2):
+ 		"""Function to get hit. 'hpm' is the amount of HP taken."""
+ 		hpm = random.randint(hpm1, hpm2)
+ 		self.hp -= hpm
+ 		cprint('\nThe {} has taken {} points of damage!'.format(colored(self.name, attrs=['bold']), colored(str(hpm), 'green', attrs=['bold'])))
+ 		if self.hp <= 0:
+ 			self.chstate('dead')
 
 
 ###############
@@ -88,23 +103,26 @@ def borg(mtrue, mfalse):
 ##############
 
 os.system('cls')
-doggy = Enemy('Tame Dog', 'A tame and cute dog. It has a pink collar.', 'alive')
+doggy = Enemy('Tame Dog', 'A tame and cute dog. It has a pink collar.', 10, 'alive')
 print('You decide to go on an adventure, even after your mom told you you\'re too weak for this. You walk far from home, with nothing but an used kitchen knife.')
 cprint('While walking, you find a {}. It has a cute pink collar.'.format(doggy.name), attrs=['bold'])
 
-dacts('KILL, AVOID, EXIT')
+dacts('HIT, AVOID, EXAMINATE, EXIT')
 
 while True:
 	choice1 = plinput()
-	if choice1.lower().startswith('kill'):
-		if choice1.lower() == 'kill tame dog':
-			doggy.chstate('dead')
-			break
-		elif choice1.lower() == 'kill':
-			cprint('\nYou must specify who to kill.', 'red', attrs=['bold'])
+	error = False
+	if choice1.lower().startswith('hit'):
+		if choice1.lower() == 'hit tame dog':
+			doggy.ghit(2, 4)
+			pass
+		elif choice1.lower() == 'hit':
+			cprint('\nYou must specify who to hit.', 'red', attrs=['bold'])
+			error = True
 			pass
 		else:
-			cprint('\nI did not get whoever you were trying to kill.', 'red', attrs=['bold'])
+			cprint('\nI did not get whoever you were trying to hit.', 'red', attrs=['bold'])
+			error = True
 			pass
 	elif choice1.lower() == 'avoid':
 		chance = borg('You successfully avoided the {}!'.format(doggy.name), 'The {} blocked the path.'.format(doggy.name))
@@ -112,11 +130,27 @@ while True:
 			break
 		elif chance == 2:
 			pass
-
+	elif choice1.lower().startswith('examinate'):
+		if choice1.lower() == 'examinate tame dog':
+			doggy.shstate()
+			pass
+		elif choice1.lower() == 'examinate':
+			cprint('\nYou must specify what to examinate.', 'red', attrs=['bold'])
+			error = True
+			pass
+		else:
+			cprint('\nI did not get whatever you were trying to examinate.', 'red', attrs=['bold'])
+			error = True
+			pass
 	elif choice1.lower().startswith('exit'):
 		cprint('\nYou exited.', 'red', attrs=['bold'])
 		exit()
 	else:
 		cprint('\nSorry, but I didn\'t understand you.', 'red', attrs=['bold'])
+		error = True
 		pass
+	if doggy.state == 'dead':
+		break
+	if error == True:
+		continue
 	turn = borg('> The dog purrs.', '> The dog mooes violently!')
